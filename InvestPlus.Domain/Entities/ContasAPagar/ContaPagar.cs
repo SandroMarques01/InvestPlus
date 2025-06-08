ee5d3c81af83f8a9e25e467841169eccbeb0ebf4
Sandro MARQUES <sandro.marques@vinci-energies.net>
2025-06-08 15:46:32 -0300

    Criação do módulo de COntas a pagar

---
+++ InvestPlus.Domain/Entities/ContasAPagar/ContaPagar.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InvestPlus.Domain.Entities
+{
+    public class ContaPagar
+    {
+        public Guid Id { get; set; }
+        public Guid LoginId { get; set; }
+        public string Descricao { get; set; } = null!;
+        public int DiaVencimento { get; set; }
+        public decimal ValorEstimado { get; set; }
+        public bool Status { get; set; }
+
+        // Se parcelada, usar Parcelas
+        public ICollection<Pagamento> Parcelas { get; set; } = new List<Pagamento>();
+    }
+}
---
+++ InvestPlus.Domain/Entities/ContasAPagar/Pagamento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace InvestPlus.Domain.Entities
+{
+    public class Pagamento
+    {
+        public Guid Id { get; set; }
+        public Guid ContaPagarId { get; set; }
+        public DateTime DataPagamento { get; set; }
+        public decimal Valor { get; set; }
+        public bool Paga { get; set; }
+
+        // Navegação
+        public ContaPagar? ContaPagar { get; set; }
+    }
+}
