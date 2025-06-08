1228e263a237def172f1d4040b4e829d56e179f2
Sandro MARQUES <sandro.marques@vinci-energies.net>
2025-06-08 14:24:35 -0300

    Criação da primeira classe e Emun.

---
+++ InvestPlus.Domain/Entities/Ativo.cs
@@ -0,0 +1,14 @@
+﻿using InvestPlus.Domain.Enums;
+
+namespace InvestPlus.Domain.Entities
+{
+    public class Ativo
+    {
+        public Guid Id { get; set; }
+        public string Codigo { get; set; } 
+        public string Nome { get; set; }
+        public TipoAtivo Tipo { get; set; }
+
+
+    }
+}
---
+++ InvestPlus.Domain/Enums/Class.cs
@@ -0,0 +1,9 @@
+﻿namespace InvestPlus.Domain.Enums
+{
+    public enum TipoAtivo
+    {
+        RendaVariavel= 1,
+        RendaFixa = 2,
+        Internacional = 3,
+    }
+}
